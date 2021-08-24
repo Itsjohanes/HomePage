3439fd36df2c837b6a248d7922b85d0c82ef5ba0
Itsjohanes <71767003+Itsjohanes@users.noreply.github.com>
2021-08-24 21:04:14 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
