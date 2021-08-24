e0ee289b54ffd8ccf5f5a36933fa7d95d28b1283
Itsjohanes <71767003+Itsjohanes@users.noreply.github.com>
2021-08-24 21:23:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include_once("materi.html");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="icon" href="johannes.png">
+    <link rel="icon" href="johannes.jpg">
     <title>Johannes Alexander Putra</title>
 </head>
 <body>
